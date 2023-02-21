c7949d323c607eeec6f7f27e5e6329decf8d6140
SHEGSZYDOO <segunbenson123@gmail.com>
2023-02-21 09:49:35 -0800

    10 top score sql file to be committed

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,7 +1,5 @@
 -- Lists all records of the table second_table.
 -- Records are ordered by descending score.
-score   name
-14      Bob;
-10      John;
-8       George;
-3       Alex;
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
