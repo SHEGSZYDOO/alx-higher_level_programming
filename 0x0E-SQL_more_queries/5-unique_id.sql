9088e1beaf86943729bf8c70f13941e26babfac2
SHEGSZYDOO <segunbenson123@gmail.com>
2023-02-21 21:37:36 -0800

    5 unique id sql file to be committed

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,3 @@
--- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
--- creates a database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- use a database
-USE hbtn_0d_usa;
+-- creates the table unique_id on your MySQL server
 -- creates a table
-CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
